6bf9fdb961d0e5fb68ffffcbf1fb81c35b95d7b1
AlwaysNight <37621115+Realangel0819@users.noreply.github.com>
2025-03-23 17:31:02 +0900

    [Silver II] Title: 에디터, Time: 40 ms, Memory: 11088 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,36 +1,80 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int n, cur;
+const int MX = 1000005;
+char dat[MX];
+int pre[MX], nxt[MX];
+int unused = 1;
+int size;
+
+void insert(int addr, int num){
+    dat[unused] = num;
+    pre[unused] = addr;
+    nxt[unused] = nxt[addr];
+    if (nxt[addr] != -1) pre[nxt[addr]] = unused;
+    nxt[addr] = unused;
+    unused ++;
+}
+
+void erase(int addr){
+    nxt[pre[addr]] = nxt[addr];
+    if(nxt[addr] != -1 ) pre[nxt[addr]] = pre[addr]; 
+}
+
+void traverse(){
+  int cur = nxt[0];
+  while(cur != -1){
+    cout << dat[cur];
+    cur = nxt[cur];
+  }
+  cout << "\n\n";
+}
+
+
+int main(void){
     ios::sync_with_stdio(0);
-    cin.tie(0);
-    list<char> li;
-    string a;
-    int temp = 0;
-    cin >> a;
-
-    for(auto s : a) li.push_back(s);
-    auto cursor = li.end();
-
-    int n;
+    //cin.tie(0);
+    fill(pre, pre+MX, -1);
+    fill(nxt, nxt+MX, -1);
+    string s;
+    char a;
+    cin >> s;
+    cur = 0;
+    for (auto q:s){
+        insert(cur,q);
+        cur++;
+    }
     cin >> n;
     while(n--){
-        char ch;
-        cin >> ch;
-
-        if(ch == 'L' && cursor != li.begin()) {
-            cursor--;
-        } else if(ch == 'D' && cursor != li.end()) {
-            cursor++;
-        } else if(ch == 'B' && cursor != li.begin()){
-            if (cursor != li.begin()) {
-                cursor = li.erase(--cursor);
+        cin >> a;
+        if (a == 'P'){
+            char add;
+            cin >> add;
+            insert(cur,add);
+            cur = nxt[cur];
+        }
+        else if(a=='L'){
+            if(pre[cur]!=-1){
+                cur=pre[cur];
             }
-        } else if(ch == 'P') {
-            char insertChar;
-            cin >> insertChar;
-            li.insert(cursor, insertChar);
+        }
+        else if(a=='D'){
+            if (nxt[cur]!=-1){
+                cur=nxt[cur];
+            }
+        }
+        else{
+            if(cur!=0){
+                erase(cur);
+                cur = pre[cur];
+            }
         }
     }
-    for(auto s: li) cout << s;
+    traverse();
+    
+    cin.get();
+    cin.get();
+
 }
+
