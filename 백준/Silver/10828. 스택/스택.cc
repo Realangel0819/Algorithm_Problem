d3ad4092f517711d9349e215dd1ddac5eb67f949
AlwaysNight <37621115+Realangel0819@users.noreply.github.com>
2025-03-25 14:42:04 +0900

    [Silver IV] Title: 스택, Time: 212 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,32 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    stack<int>stack;
-    int n;
+
+int n, temp;
+string op;
+int main() {
+    ios::sync_with_stdio(0);
     cin >> n;
+
+    stack<int> s;
+
     while(n--){
-        string ch;
-        cin >> ch;
-        if(ch == "push"){
-            int k;
-            cin >> k;
-            stack.push(k);
-        }else if(ch == "pop"){
-            if(stack.empty()){
-                cout << -1 << endl;
-            }else{
-                cout << stack.top() << endl;
-                stack.pop();
-            }    
-        }else if(ch=="size"){
-            cout << stack.size() << endl;
-        }else if(ch=="empty"){
-            cout << (int)stack.empty() << endl;
-        }else {
-            if(stack.empty()){
-                cout << -1 << endl;
-            }else 
-                cout << stack.top() << endl;
-        }     
+        cin >> op;
+        if (op =="push"){
+            cin >> temp;
+            s.push(temp);
+        }
+        else if(op =="pop"){
+            if(!s.empty()) {
+                cout << s.top()<< '\n';
+                s.pop();
+            }
+            else cout << -1<< '\n';
+        }
+        else if(op =="size"){
+            cout << s.size()<< '\n';
+        }
+        else if(op =="empty"){
+            if (s.empty()) cout << 1<< '\n';
+            else cout << 0<< '\n';
+        }
+        else{
+            if (!s.empty()) cout << s.top()<< '\n';
+            else cout << -1<< '\n';
+        }
     }
 }
